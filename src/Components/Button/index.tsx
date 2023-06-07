d3710f7937ef8416e6800e7b95d330a06c5cbafc
Max Kikuchi <maxkikuchi@hotmail.com>
2023-06-07 19:42:59 -0300

    - Instalação do pacote npm "Sass" (css) - Estilizado com Saas

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,13 +1,58 @@
 import React from "react";
+import './style.scss'
 
-class Button extends React.Component{
+class ButtonProd extends React.Component{
     render() {
         return(
-            <button>
+            <button className="button">
                 Button
             </button>
         )
     }
 } 
 
-export default Button;
+//Exemplo 1
+class ButtonTeste1 extends React.Component{
+    render() {
+        const backgroundColor = 'blue';
+        const styles = {
+            backgroundColor
+        }        
+
+        return(
+            <button style={styles}>
+                Button
+            </button>
+        )
+    }
+}
+
+//Exemplo 2
+class ButtonTeste2 extends React.Component{
+    render() {
+        
+        return(
+            <button style={{color: "orange"}}>
+                Button
+            </button>
+        )
+    }
+} 
+
+//Exemplo 3
+class ButtonTeste3 extends React.Component{
+    render() {
+        const estaAtivo = false;
+        const styles = {
+            backgroundColor: estaAtivo ? "green" : "red"
+        }        
+                
+        return(
+            <button style={styles}>
+                Button
+            </button>
+        )
+    }
+}
+
+export default ButtonProd
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import Form from '../Components/Form';
 import List from '../Components/List'
+import './style.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div className="AppStyle">
+      
+      <List />
+    </div>
+  );
+}
+
+function App2() {
+  return (
+    <div className="AppStyle">
       <Form />
       <List />
     </div>
   );
 }
 
-export default App;
+export default App2;
