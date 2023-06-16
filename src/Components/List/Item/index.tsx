d3ebd68e44b6a433bc9f494c7b93e8217e527d0a
Max Kikuchi <maxkikuchi@hotmail.com>
2023-06-16 11:07:19 -0300

    - Controlando form - Adicionando tarefa - Atualizando tarefa

--- src/Components/List/Item/index.tsx
+++ src/Components/List/Item/index.tsx
@@ -1,14 +1,13 @@
+import { ITarefa } from '../../../types/tarefa'
 import style from '../list.module.scss'
 
-export default function Item({tarefa, tempo} : {tarefa: string, tempo: string}){
+export default function Item({tarefa, tempo, selecionado, completado, id} : ITarefa){
+    console.log('tarefa atual:', {tarefa, tempo, selecionado, completado, id})
+
     return(
         <li className={style.item}>
-            <h3>
-                {tarefa}
-            </h3>
-            <span>
-                {tempo}
-            </span>
+            <h3>{tarefa}</h3>
+            <span>{tempo}</span>
         </li>
     )
 }
